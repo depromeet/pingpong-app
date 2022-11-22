25fa0800da72cf3818575a9d2e92b16c9096a2ac
L2HYUNN <donghyuni98@gmail.com>
2022-11-22 20:10:31 +0900

    :sparkles: feat(app): Add default example using WebView
    webview기본 예시 추가 (statusBar, SafeAreaView 적용)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-/* eslint-disable import/no-unresolved */
-import { TEST_URL } from '@env';
+import { BASE_URL } from '@env';
 import React from 'react';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const App = () => {
-  return <WebView source={{ uri: TEST_URL }} />;
+  return (
+    <>
+      <StatusBar barStyle="default" />
+      <SafeAreaView style={styles.container}>
+        <WebView source={{ uri: BASE_URL }} />
+      </SafeAreaView>
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
