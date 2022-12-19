c195daaf6ae8db5c73374e9fdde45b6469af28d5
Hwang yena <center7332@naver.com>
2022-12-20 00:58:15 +0900

    :bug: fix: ios build error

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,27 +1,33 @@
 import messaging from '@react-native-firebase/messaging';
-import React, { useEffect } from 'react';
-import { Alert, SafeAreaView } from 'react-native';
-import { Header } from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useEffect } from 'react';
+import { Alert } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  // const requestUserPermission = async () => {
-  //   const authStatus = await messaging().requestPermission();
-  //   const enable =
-  //     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-  //     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enable =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-  //   if (enable) {
-  //     console.log('Authorization status: ', authStatus);
-  //   }
-  // };
+    if (enable) {
+      console.log('Authorization status: ', authStatus); //1: access
+    }
+  };
 
-  // useEffect(() => {
-  //   requestUserPermission();
-  // }, []);
+  useEffect(() => {
+    requestUserPermission();
+  }, []);
 
   //Foreground
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      console.log('>> A new FCM message arrived!');
       Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
     });
 
@@ -29,9 +35,13 @@ const App = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <Header />
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 };
 
