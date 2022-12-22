8b61862570509cc3dcc2272ae0ae381b8e4e56c1
Hwang yena <center7332@naver.com>
2022-12-23 01:03:08 +0900

    :sparkles: feat: refreshcontrol

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@
 
 import messaging from '@react-native-firebase/messaging';
 import { AppRegistry } from 'react-native';
-import App from './src/pages/App';
+import App from './src/App';
 import { name as appName } from './app.json';
 
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
