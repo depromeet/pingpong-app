06daec141545aac6e367b554c8a3314e87147683
Hwang yena <center7332@naver.com>
2023-01-05 23:34:11 +0900

    :sparkles: feat: Change name to pingpong

--- ios/pingpong/AppDelegate.mm
+++ ios/pingpong/AppDelegate.mm
@@ -48,7 +48,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"winwin_app", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"pingpong", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
